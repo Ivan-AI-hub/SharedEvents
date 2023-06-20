557451a1b1d4c12afb48916c4fb465e521cd161a
Ivan Andreevets <ivan_grozdny@mail.ru>
2023-06-20 12:05:35 +0300

    Add UserRoleUpdated event

---
+++ SharedEvents/SharedEvents.Models/UserRoleUpdated.cs
@@ -0,0 +1,13 @@
+﻿namespace SharedEvents.Models
+{
+    public class UserRoleUpdated
+    {
+        public string Email { get; set; }
+        public string Role { get; set; }
+        public UserRoleUpdated(string email, string role)
+        {
+            Email = email;
+            Role = role;
+        }
+    }
+}
